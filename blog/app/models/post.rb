a97f5f7741e4ed3033e97c9f6c477d1f5d348508
Madhu-Lumiseek <madhu.k@lumiseek.com>
2020-10-14 16:18:44 +0530

    Task-3

---
+++ blog/app/models/post.rb
@@ -0,0 +1,20 @@
+class Post
+  include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
+  include Mongoid::Timestamps::Created
+  include Mongoid::Timestamps::Updated
+  # field :id, type: String
+  field :author_name, type: String
+  # validates :name, format: {with: /[a-zA-Z]/}, presence: true
+  field :title, type: String
+  validates :title, format: {with: /[a-zA-Z]/}, presence: true
+  field :genre, type: String
+  validates :genre, format: {with: /[a-zA-Z]/}, presence: true
+  field :noofpages, type: Integer
+  validates :noofpages, presence: true
+  field :description, type: String
+  validates :description, format: {with: /[a-zA-Z]/}, presence: true
+
+  belongs_to :author, :optional => true
+  validates_presence_of :author
+end
